f993f9fa61ac91ca50378cf36c68009ba5141c16
Raja Sihombing <if09051@gmail.com>
2020-10-26 22:16:08 +0800

    Add employee class

---
+++ src/main/java/com/softhaxi/marves/core/domain/account/Employee.java
@@ -0,0 +1,49 @@
+package com.softhaxi.marves.core.domain.account;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+
+/**
+ * @author Raja Sihombing
+ * @since 1
+ */
+@Entity
+@Table(name = "employees")
+@Access(value = AccessType.FIELD)
+public class Employee implements Serializable {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "org.hibernate.id.UUIDGenerator")
+    @Type(type = "pg-uuid")
+	@Column(name = "id", updatable = false, nullable = false)
+    protected UUID id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    protected User user;
+
+    @Column(name = "employee_no", nullable = false, length = 50)
+    protected String employeeNo;
+    
+
+}
