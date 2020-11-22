d5e95dbd3a37ff3eb594f8b2cae131c11fada07b
Raja Sihombing <if09051@gmail.com>
2020-11-22 23:28:47 +0800

    Auditing if login from web using LDAP

--- src/main/java/com/softhaxi/marves/core/domain/AuditorAwareImpl.java
+++ src/main/java/com/softhaxi/marves/core/domain/AuditorAwareImpl.java
@@ -4,6 +4,8 @@ import java.util.Optional;
 
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.ldap.userdetails.LdapUserDetails;
+import org.springframework.security.ldap.userdetails.LdapUserDetailsImpl;
 
 public class AuditorAwareImpl implements AuditorAware<String> {
 
@@ -14,8 +16,19 @@ public class AuditorAwareImpl implements AuditorAware<String> {
         }
         Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         
+        if(principal != null) {
+            if(principal instanceof LdapUserDetailsImpl) {
+                LdapUserDetails ldapUser = (LdapUserDetailsImpl) principal;
+                return Optional.of(ldapUser.getUsername());
+            }
+        }
         return (principal == null || principal.toString().equalsIgnoreCase("anonymousUser")) 
             ? Optional.of("SYSTEM") : Optional.of(principal.toString());
+    }
+
+    @Override
+    public String toString() {
+        return "AuditorAwareImpl []";
     }
     
 }
