a612732845c37e0615e50806b41ae080b5e12547
Softh Axi <softhaxi@gmail.com>
2021-02-17 14:25:35 +0700

    Add dispensation detail of employee

--- src/main/java/com/softhaxi/marves/core/repository/attendance/DispensationRepository.java
+++ src/main/java/com/softhaxi/marves/core/repository/attendance/DispensationRepository.java
@@ -13,17 +13,14 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface DispensationRepository extends JpaRepository<Dispensation, UUID> {
     public Collection<Dispensation> findAllByUserOrderByStartDateDesc(User user);
-    
-    @Query("FROM Dispensation " +
-        " WHERE user = ?1 AND startDate <= ?2 AND endDate >= ?2")
+
+    @Query("FROM Dispensation " + " WHERE user = ?1 AND startDate <= ?2 AND endDate >= ?2")
     public Collection<Dispensation> findByUserAndDate(User user, LocalDate date);
 
-    @Query("FROM Dispensation " +
-        " WHERE user = ?1 AND (startDate < ?2 OR endDate < ?2) ORDER BY startDate DESC")
+    @Query("FROM Dispensation " + " WHERE user = ?1 AND (startDate < ?2 OR endDate < ?2) ORDER BY startDate DESC")
     public Collection<Dispensation> findAllByUserAndBeforeDate(User user, LocalDate date);
 
-    @Query("FROM Dispensation " +
-        " WHERE user = ?1 AND startDate >= ?2 AND endDate <= ?3")
+    @Query("FROM Dispensation " + " WHERE user = ?1 AND startDate >= ?2 AND endDate <= ?3")
     public Collection<Dispensation> findByUserAndBetweenDates(User user, LocalDate start, LocalDate end);
 
     @Query("FROM Dispensation WHERE CURRENT_DATE >= startDate  AND CURRENT_DATE <= endDate")
@@ -38,9 +35,14 @@ public interface DispensationRepository extends JpaRepository<Dispensation, UUID
     @Query("SELECT COUNT(*) FROM Dispensation WHERE type = ?1 AND startDate <= ?2 AND endDate >= ?2")
     public Object findStatisticByTypeAndDate(String type, LocalDate date);
 
-    @Query("SELECT startDate, endDate, COUNT(startDate) " +
-        " FROM Dispensation WHERE startDate >= ?1 AND endDate <= ?2" + 
-        " GROUP BY startDate, endDate ORDER BY startDate")
+    @Query("SELECT COUNT(*) FROM Dispensation WHERE user = ?1 AND type = ?2 AND (EXTRACT(YEAR FROM startDate) = ?3 OR EXTRACT(YEAR FROM endDate) = ?3)")
+    public Object findStatisticByUserAndTypeAndYear(User user, String type, int year);
+
+    @Query("SELECT startDate, endDate, COUNT(startDate) " + " FROM Dispensation WHERE startDate >= ?1 AND endDate <= ?2"
+            + " GROUP BY startDate, endDate ORDER BY startDate")
     public Collection<Object[]> findStatisticFromRangeDate(LocalDate start, LocalDate end);
+
+    @Query("SELECT a FROM Dispensation a JOIN User b ON b.id=a.user.id WHERE a.startDate <= ?1 AND a.endDate >= ?1 AND b.email IN (?2)")
+    public Collection<Dispensation> findAllByDateAndEmails(LocalDate date, Collection<String> emails);
 
 }
