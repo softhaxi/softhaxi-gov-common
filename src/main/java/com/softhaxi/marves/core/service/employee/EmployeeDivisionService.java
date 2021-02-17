fed909707711dfa13245014e17e5d68b1109d5fc
Softh Axi <softhaxi@gmail.com>
2021-02-17 14:24:44 +0700

    Implement List of employee based on group

--- src/main/java/com/softhaxi/marves/core/service/employee/EmployeeDivisionService.java
+++ src/main/java/com/softhaxi/marves/core/service/employee/EmployeeDivisionService.java
@@ -37,6 +37,12 @@ public class EmployeeDivisionService {
         entry("JENIS_ES1", "type")
     );
 
+    private Map<Object, Object> employeeMapping = Map.ofEntries(
+        entry("EMAIL", "email"),
+        entry("NAMAPENDEK", "name"),
+        entry("UNITKERJA", "division")
+    );
+
     public Collection<Map<?, ?>> findAll() {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.MULTIPART_FORM_DATA);
@@ -62,6 +68,37 @@ public class EmployeeDivisionService {
         result.forEach(item -> {
             Map<Object, Object> temp = new HashMap<>();
             divisionMapping.entrySet().forEach(entry -> temp.put(entry.getValue().toString(), item.get(entry.getKey())));
+            data.add(temp);
+        });
+        return data;
+    }
+
+    public Collection<Map<?, ?>> findEmployeeByDivision(String id) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+        String token = marvesHRUtil.getAccessToken();
+        if(token == null) {
+            return null;
+        }
+
+        headers.add("token", token);
+        MultiValueMap<String, Object> form = new LinkedMultiValueMap<>();
+        form.add("api", "get_user_by_group");
+        form.add("ESELON1_ID", id);
+        HttpEntity<Map<?, ?>> entity = new HttpEntity<>(form, headers);
+        ResponseEntity<?> response = restTemplate.postForEntity(String.format("%s%s", marvesHRUtil.getBaseUrl(), marvesHRUtil.getApiEndpoint()), 
+            entity, 
+            Map.class);
+        if(response.getStatusCode() != HttpStatus.OK) {
+            return null;
+        }
+        Map<?, ?> body = (Map<?, ?>) response.getBody();
+        List<Map<?, ?>> result = (List<Map<?, ?>>) body.get("result");
+        List<Map<?, ?>> data = new LinkedList<>();
+        
+        result.forEach(item -> {
+            Map<Object, Object> temp = new HashMap<>();
+            employeeMapping.entrySet().forEach(entry -> temp.put(entry.getValue().toString(), item.get(entry.getKey())));
             data.add(temp);
         });
         return data;
