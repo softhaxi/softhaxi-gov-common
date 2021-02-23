26ab965b9152619f3a3345bbbb15f960c193f720
Softh Axi <softhaxi@gmail.com>
2021-02-23 16:38:52 +0700

    Implement session for check daily login

--- src/main/java/com/softhaxi/marves/core/repository/logging/SessionRepository.java
+++ src/main/java/com/softhaxi/marves/core/repository/logging/SessionRepository.java
@@ -1,5 +1,6 @@
 package com.softhaxi.marves.core.repository.logging;
 
+import java.time.ZonedDateTime;
 import java.util.Collection;
 import java.util.Optional;
 import java.util.UUID;
@@ -15,8 +16,14 @@ public interface SessionRepository extends JpaRepository<Session, UUID> {
     public Collection<Session> findAllValidByUser(User user);
 
     @Query("FROM Session WHERE accessToken = ?1 AND Status='VALID'")
-    public Optional<Session> findAllValidByAccessToken(String accessToken);
+    public Optional<Session> findOneValidByAccessToken(String accessToken);
+
+    @Query("FROM Session WHERE UPPER(status) = UPPER(?1) ORDER BY updatedAt DESC")
+    public Collection<Session> findAllByStatus(String status);
+
+    @Query("FROM Session WHERE UPPER(status) = UPPER(?1) AND lastUsed >= ?2 AND lastUsed <= ?3 ORDER BY updatedAt DESC")
+    public Collection<Session> findAllByStatusAndDateRange(String status, ZonedDateTime start, ZonedDateTime end);
     
-    @Query("SELECT COUNT(*) FROM Session WHERE status='VALID'")
-    public Object findStatisticValidSession();
+    @Query("SELECT COUNT(*) FROM Session WHERE UPPER(status) = UPPER(?1) AND lastUsed >= ?2 AND lastUsed <= ?3")
+    public Object findStatisticStatusSession(String status, ZonedDateTime start, ZonedDateTime end);
 }
